bcd01fa4631c34d011e90a80f92c73797ddd4f26
Rikkert Koppes <rikkert@rikkertkoppes.com>
2016-11-24 18:11:04 +0100

    do handshake to new camarillo

--- index.js
+++ index.js
@@ -3,14 +3,41 @@ var n = new nfc();
 var exec = require('child_process').exec;
 var path = require('path');
 var config = require('./config.json');
+var request = require('request');
 
 var Pusher = require('pusher');
  
-var pusher = new Pusher(config);
+var pusher = new Pusher(config.pusher);
 
 var currentTag;
 var removeDelay = 100;
 var removeTimer;
+var endpoint = config.camarillo.endpoint + 'controls/';
+var key = config.camarillo.key;
+
+function checkPerformance(uid) {
+	var url = endpoint + 'parts?key='+key+'&performances.member.meta.uuid='+uid
+	request.get(url, function(err, response, body) {
+		var parts = JSON.parse(body);
+		var performances = parts.reduce(function(all, part) {
+			return all.concat(part.performances.filter(function(performance) {
+				return performance.member.meta.uuid == uid;
+			}));
+		}, []);
+		performances.forEach(function(performance) {
+			var url = endpoint + 'parts/'+performance.partId+'/performances/'+performance.id+'/status?key='+key;
+			var status = performance.meta.status === 'done_all'?'done_all':'done';
+			var name = [performance.member.firstName,performance.member.infix,performance.member.lastName].join(' ');
+			console.log('set',name,'to',status);
+			request.put(url, {
+				body: {
+					status: status
+				},
+				json: true
+			});
+		});
+	});
+}
 
 function beep() {
 	console.log('beep');
@@ -46,8 +73,11 @@ function handleRemove() {
 }
 
 function handleEvent(e) {
-	//push to pusher
-	pusher.trigger('camarillo', 'scan-uid', e);
+	if (e.type === 'detect') {
+		//push to pusher
+		pusher.trigger('camarillo', 'scan-uid', e);
+		checkPerformance(e.uid);
+	}
 	console.log(JSON.stringify(e));
 }
 
